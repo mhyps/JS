3088502db40760c3fe27ca87892f8bb85d3f1300
mhyps <hypsmateusz@gmail.com>
2019-08-17 21:01:30 +0200

    adding key listener

--- Snake/script.js
+++ Snake/script.js
@@ -1,7 +1,6 @@
 //Creating rectangle to fill canvas
 const gameCanvas = document.querySelector("#gameCanvas");
 const gameCanvasContext = gameCanvas.getContext("2d");
-
 let snake = [
     {x: 200, y: 200},
     {x: 190, y: 200},
@@ -9,9 +8,12 @@ let snake = [
 ];
 
 let dx = 10;
+
 let dy = 0;
 
 main();
+
+document.addEventListener("keydown", changeDirection);
 
 function main() {
     setTimeout(function onTick() {
@@ -19,7 +21,7 @@ function main() {
         advanceSnake();
         drawSnake();
         main();
-    },100);
+    }, 100);
 }
 
 function clearCanvas() {
@@ -35,6 +37,37 @@ function drawSnakePart(snakePart) {
     gameCanvasContext.fillStyle = "#a993c2";
     gameCanvasContext.fillRect(snakePart.x, snakePart.y, 10, 10);
     gameCanvasContext.strokeRect(snakePart.x, snakePart.y, 10, 10);
+
+}
+
+function changeDirection(event) {
+    const LEFT_KEY = "ArrowLeft";
+    const RIGHT_KEY = "ArrowRight";
+    const UP_KEY = "ArrowUp";
+    const DOWN_KEY = "ArrowDown";
+
+    const keyPressed = event.key;
+    const goingUp = dy === -10;
+    const goingDown = dy === 10;
+    const goingRight = dx === 10;
+    const goingLeft = dx === -10;
+
+    if (keyPressed === LEFT_KEY && !goingRight) {
+        dx = -10;
+        dy = 0;
+    }
+    if (keyPressed === RIGHT_KEY && !goingLeft) {
+        dx = 10;
+        dy = 0;
+    }
+    if (keyPressed === UP_KEY && !goingDown) {
+        dx = 0;
+        dy = -10;
+    }
+    if (keyPressed === DOWN_KEY && !goingUp) {
+        dx = 0;
+        dy = 10;
+    }
 
 }
 
