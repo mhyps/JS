ede36b06cd617935f6a5a936006cdf2812218719
mhyps <hypsmateusz@gmail.com>
2019-08-18 21:14:04 +0200

    fixing bugs

--- memoryGame/script.js
+++ memoryGame/script.js
@@ -1,20 +1,57 @@
 const cards = document.querySelectorAll(".memory-card");
 
 let hasFlippedCard = false;
-let firstCard,secondCard;
+let lockBoard = false;
+let firstCard, secondCard;
 
-function flipcard(){
+function flipcard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
     this.classList.add('flip');
 
-    if (!hasFlippedCard){
+    if (!hasFlippedCard) {
         //getting first card
         hasFlippedCard = true;
         firstCard = this;
-    }else {
-        //getting second card
-        hasFlippedCard = false;
-        secondCard = this;
+
+        return;
     }
+    //getting second card
+    secondCard = this;
+
+    // do cards match
+    checkForMatch();
+
 }
 
-cards.forEach(card => card.addEventListener("click",flipcard));
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipcard());
+    secondCard.removeEventListener('click', flipcard());
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+cards.forEach(card => card.addEventListener("click", flipcard));
