28481d31f57c2b82c6a6a6785e802f086641dcd5
mhyps <hypsmateusz@gmail.com>
2019-08-18 20:39:49 +0200

    flip click function

--- memoryGame/script.js
+++ memoryGame/script.js
@@ -1,8 +1,20 @@
 const cards = document.querySelectorAll(".memory-card");
 
+let hasFlippedCard = false;
+let firstCard,secondCard;
 
 function flipcard(){
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if (!hasFlippedCard){
+        //getting first card
+        hasFlippedCard = true;
+        firstCard = this;
+    }else {
+        //getting second card
+        hasFlippedCard = false;
+        secondCard = this;
+    }
 }
 
 cards.forEach(card => card.addEventListener("click",flipcard));
